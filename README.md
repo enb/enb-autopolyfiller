bedbb0d8516d0465b3c025bc3c88bf31632c9d35
Marat Dulin <mdevils@yandex.ru>
2014-05-05 12:33:00 +0400

    1.0.0: Changelog

--- README.md
+++ README.md
@@ -12,12 +12,14 @@ autopolyfiller
 
 * *String* **target** — Результирующий таргет. По умолчанию — `?.js`.
 * *String* **source** — Исходный файл. По умолчанию — `?.source.js`.
-* *String* **autopolyfillerArguments** — Аргументы автополифилера.
+* *String[]* **browsers** — Браузеры, для которых необходимо построить полифилы.
+* *String[]* **excludes** — Полифилы, которые необходимо исключить из сборки.
+* *String[]* **includes** — Полифилы, которые необходимо включить в сборку.
 
 **Пример**
 
 ```javascript
 nodeConfig.addTechs([
-  [ require('autopolyfiller'), {autopolyfillerArguments: ['IE 11', 'Chrome >= 31']} ]
+    [require('autopolyfiller'), {browsers: ['IE 11', 'Chrome >= 31']}]
 ]);
 ```
--- techs/autopolyfiller.js
+++ techs/autopolyfiller.js
@@ -8,13 +8,15 @@
  *
  * * *String* **target** — Результирующий таргет. По умолчанию — `?.js`.
  * * *String* **source** — Исходный файл. По умолчанию — `?.source.js`.
- * * *String* **autopolyfillerArguments** — Аргументы автополифилера.
+ * * *String[]* **browsers** — Браузеры, для которых необходимо построить полифилы.
+ * * *String[]* **excludes** — Полифилы, которые необходимо исключить из сборки.
+ * * *String[]* **includes** — Полифилы, которые необходимо включить в сборку.
  *
  * **Пример**
  *
  * ```javascript
  * nodeConfig.addTechs([
- *   [ require('autopolyfiller'), {autopolyfillerArguments: ['IE 11', 'Chrome >= 31']} ]
+ *     [require('autopolyfiller'),{browsers: ['IE 11', 'Chrome >= 31']}]
  * ]);
  * ```
  */
@@ -23,9 +25,15 @@ var autopolyfiller = require('autopolyfiller');
 module.exports = require('enb/lib/build-flow').create()
     .name('autopolyfiller')
     .target('target', '?.js')
-    .defineOption('autopolyfillerArguments', [])
     .useSourceText('source', '?.source.js')
+    .defineOption('browsers', [])
+    .defineOption('excludes', [])
+    .defineOption('includes', [])
     .builder(function (source) {
-        return autopolyfiller.call(null, this._autopolyfillerArguments).add(source) + source;
+        return autopolyfiller
+            .call(null, this._browsers)
+            .exclude(this._excludes)
+            .include(this._includes)
+            .add(source) + source;
     })
     .createTech();
